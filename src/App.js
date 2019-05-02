04ef3d822a45c227230b7feab7fd875417f4af63
Matt Raines <mraines4@gmail.com>
2019-05-02 15:41:20 -0400

    form calls handler function

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import axios from 'axios';
 import ChatList from './ChatList';
+import ChatForm from './ChatForm';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      messages: []
-    }
+      messages: [],
+      text: ''
+    };
   }
 
   componentDidMount() {
     setInterval(async () => {
       const {data} = await axios.get('/api')
-      console.log(data);
+      // console.log(data);
       this.setState({
         messages: data
       });
@@ -25,8 +27,20 @@ class App extends React.Component {
       <div className="App">
         <h1>Chat App</h1>
         <ChatList messages={this.state.messages} />
+        <ChatForm text={this.state.text} handleChange={this._setText} handleSend={this._sendMessage} />
       </div>
     );
+  }
+
+  _setText = (text) => {
+    console.log('setting text');
+    this.setState({
+      text
+    })
+  }
+
+  _sendMessage = () => {
+    console.log('sending message');
   }
 }
 
--- src/ChatForm.js
+++ src/ChatForm.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 function ChatForm({text, handleChange, handleSend}) {
     return (
-        <form onSubmit={handleSend}>
-            <input value={text} onChange={handleChange} />
-            <button onClick={handleSend} >Send</button>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            handleSend()
+            }}>
+            <input value={text} onChange={e => handleChange(e.target.value)} />
+            <button >Send</button>
         </form>
     )
 }
